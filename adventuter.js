f9a2a9a69ce1acc3ac0ad5fc6650b86d53ef7bcd
ldcqkqn <40484355+ldcqkqn@users.noreply.github.com>
2019-11-15 02:37:27 +0900

    practice_2

---
+++ adventuter.js
@@ -0,0 +1,17 @@
+function siteClick(urlname){
+    if(urlname === 'google'){
+        location.href = 'https://www.google.com/';
+    }else if(urlname === 'naver'){
+        location.href = "https://www.naver.com";    
+    }
+}
+
+var imglinked = function(){
+    var imgs = document.getElementsByClassName('imgs');
+    var i;
+    var imgbox = [];
+    for(i = 0; i < imgs.length; i++){
+        imgs[i].
+    }
+}   
+imglinked();
